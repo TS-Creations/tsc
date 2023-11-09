b648b56775f3c4f93586d6bfb9178d8f4bd61146
SathuMathu <sathursanmathu@gmail.com>
2023-11-09 13:49:15 +0530

    up

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>KSS Stores</title>
+    <title>TS Creations</title>
 </head>
 <body>
     
