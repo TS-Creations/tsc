4ee87c54e1d45c9049a33deb28be6d78fa4595e0
SathuMathu <sathursanmathu@gmail.com>
2023-11-11 01:05:28 +0530

    up

--- js/tsscript.js
+++ js/tsscript.js
@@ -3,17 +3,22 @@ $(function () {
   $("#includenav").load("nav.html");
 });
 
+$(document).ready(function(){
+  $('#head').load("head.html");
+  $('#foot').load("foot.html");
+});
+
 //nav include sub
 $(function () {
   $("#includenavsub").load("sub/nav.html");
 });
 
-//include head
+//include head sub
 $(function () {
   $("#includeheadsub").load("sub/head.html");
 });
 
-//include foot
+//include foot sub
 $(function () {
   $("#includefootsub").load("sub/foot.html");
 });
